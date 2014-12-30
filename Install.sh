e690eb86f6c3678399085171fd63418ccacc57fe
NotSoSolidSnake <dexcasul@zoho.com>
2014-12-29 20:57:22 -0600

    Added main file, some echo messages, and shitty code.

---
+++ Install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Welcome to my install scripts"
+echo "The goal of this program is to be able to install any program that comes packaged in a tar.gz format."
+
+echo "Let's start by getting to your downloads directory"
+echo "Note: please have the archive installed already"
+cd ~/Downloads/
